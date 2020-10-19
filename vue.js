8243ea4f53b8a504a7831a8965c66d3e781d8285
sebastian <sebastian.schmidt@ot-movimento.de>
2020-10-19 14:57:21 +0200

    add vue but compiler missing

---
+++ vue.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+}
+})
+
+const app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+}
+})
+
+const app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+}
+})
+
+const app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
